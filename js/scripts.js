561964acfd63822b1526ea225c52e8111017dc1d
jgeihsler <86137411+jgeihsler@users.noreply.github.com>
2021-07-20 14:16:24 -0500

    Navtoggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hi Welcome to my Portfolio")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
